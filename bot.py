c2b01df1e9745db52fd370eabedef017c009928f
Chiru <hasenorc@gmail.com>
2025-04-18 11:57:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import random
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application
+import os
 
-# Function to handle the /quote command
+YOUR_TOKEN = "7854424887:AAGa42aSgHDInjkWMuVknJuRugQSpB2QtTY" 
 async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     quotes = [
         "The only limit to our realization of tomorrow is our doubts of today.",
@@ -14,14 +14,15 @@ async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(selected_quote)
 
 def main() -> None:
-    # Replace 'YOUR_TOKEN' with your bot's token
     application = ApplicationBuilder().token("7854424887:AAGa42aSgHDInjkWMuVknJuRugQSpB2QtTY").build()
 
-    # Register the command handler
     application.add_handler(CommandHandler("quote", quote))
 
-    # Start the Bot
-    application.run_polling()
+    # Set a webhook URL
+    webhook_url = os.getenv('WEBHOOK_URL')  # Set this in Koyeb's environment variables
+    application.run_webhook(listen='0.0.0.0', port=int(os.getenv('PORT', '8443')), url_path='7854424887:AAGa42aSgHDInjkWMuVknJuRugQSpB2QtTY')
+    application.bot.set_webhook(f"{webhook_url}/{YOUR_TOKEN}")
 
 if __name__ == '__main__':
     main()
+    
